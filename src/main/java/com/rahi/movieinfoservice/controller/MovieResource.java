028ffe64e43cff4957ca0300acc1bbe9f8a1b96e
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-18 21:49:10 +0600

    Added external movie db

--- src/main/java/com/rahi/movieinfoservice/controller/MovieResource.java
+++ src/main/java/com/rahi/movieinfoservice/controller/MovieResource.java
@@ -1,10 +1,13 @@
 package com.rahi.movieinfoservice.controller;
 
 import com.rahi.movieinfoservice.model.Movie;
+import com.rahi.movieinfoservice.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @Author Amimul Ehsan
@@ -16,9 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("movies")
 public class MovieResource {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+    public MovieResource( RestTemplate restTemplate ) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("{movieId}")
     public Movie getMovieInfo( @PathVariable("movieId") String movieId ) {
-        return new Movie(movieId, "Test name");
+
+        MovieSummary movieSummary = restTemplate.getForObject(
+                "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey,
+                MovieSummary.class
+        );
+
+        assert movieSummary != null;
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 
 }
---
+++ src/main/java/com/rahi/movieinfoservice/model/MovieSummary.java
@@ -0,0 +1,47 @@
+package com.rahi.movieinfoservice.model;
+
+/**
+ * @Author Amimul Ehsan
+ * @Created at 10/18/21
+ * @Project Movie-Info-Service
+ */
+
+public class MovieSummary {
+
+    private String id;
+    private String title;
+    private String overview;
+
+    public MovieSummary() {
+    }
+
+    public MovieSummary( String id, String title, String overview ) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId( String id ) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle( String title ) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview( String overview ) {
+        this.overview = overview;
+    }
+}
